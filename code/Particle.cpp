e51c3388c3bb7845d63dd5adee8168a497fea074
Mario <150199104+mariomorales0@users.noreply.github.com>
2023-11-28 20:33:53 -0800

    Define functions in Particle.cpp

--- code/Particle.cpp
+++ code/Particle.cpp
@@ -144,3 +144,27 @@ void Particle::unitTests()
 
     cout << "Score: " << score << " / 7" << endl;
 }
+
+Particle::Particle(RenderTarget &target, int numPoints, Vector2i mouseClickPosition) {
+
+}
+
+void Particle::draw(RenderTarget &target, RenderStates states) const {
+
+}
+
+void Particle::update(float dt) {
+
+}
+
+void Particle::translate(double xShift, double yShift) {
+
+}
+
+void Particle::rotate(double theta) {
+
+}
+
+void Particle::scale(double c) {
+
+}
